f2d2cc65181b9c4a0a1558e66ef28adbedad480e
Samantha Morco <samanthamorco@gmail.com>
2017-04-20 15:50:14 -0700

    flash messages are buggy for some reason

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :desc)
     render 'index.json.jbuilder'
   end
 end
