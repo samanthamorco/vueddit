14647541dcc19a4d565198219e7bf71aaeb68695
Samantha Morco <samanthamorco@gmail.com>
2017-04-20 14:54:17 -0700

    post model and basic create function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,25 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      flash[:success] = 'Post Created'
+      redirect_to root_path
+    else
+      flash[:error] = '<ul>' + @post.errors.full_messages.map { |msg| "<li>#{msg}</li>" }.join(' ') + '</ul>'
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
